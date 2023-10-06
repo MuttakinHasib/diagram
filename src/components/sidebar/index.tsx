32ec367dc951d6bf4baba0b0019d2c10ebb37928
Muttakin Islam Hasib <muttakinislamhasib@gmail.com>
2023-10-06 16:21:59 +0600

    feat(web): :hammer: enabled typed route

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,11 +1,15 @@
+import Link from "next/link";
 import React from "react";
 
 const Sidebar = () => {
   return (
-    <aside className="w-72">
-      <h2 className="h-20 grid place-content-center text-center font-bold text-3xl bg-primary text-white">
+    <aside className="w-72 min-h-screen bg-gray-100">
+      <Link
+        href="/"
+        className="h-20 grid place-content-center text-center font-bold text-3xl bg-primary text-white"
+      >
         Logo
-      </h2>
+      </Link>
     </aside>
   );
 };
