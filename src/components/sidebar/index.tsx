904b17dc6b73d8e8717dbed30d73d2641754749a
Muttakin Islam Hasib <muttakinislamhasib@gmail.com>
2023-10-07 15:56:45 +0600

    feat(web): :hammer: add sidebar navigation

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,5 +1,13 @@
+import { sidebarNavigation } from "@/configs";
 import Link from "next/link";
 import React from "react";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "../ui/accordion";
+import { Input } from "../ui/input";
 
 const Sidebar = () => {
   return (
@@ -11,6 +19,28 @@ const Sidebar = () => {
         Logo
       </Link>
       <h3 className="py-3 text-center text-xl font-bold">Categories</h3>
+      <div className="flex flex-col gap-y-3 p-5">
+        <Accordion type="single" collapsible>
+          {sidebarNavigation.map((navigation, index) => (
+            <AccordionItem key={index} value={navigation.label}>
+              <AccordionTrigger>{navigation.label}</AccordionTrigger>
+
+              <AccordionContent>
+                <div className="flex flex-col gap-2 p-1 pb-4">
+                  {navigation.children.map((child, index) => (
+                    <Input
+                      key={child.name}
+                      type="text"
+                      name={child.name}
+                      placeholder={child.name}
+                    />
+                  ))}
+                </div>
+              </AccordionContent>
+            </AccordionItem>
+          ))}
+        </Accordion>
+      </div>
     </aside>
   );
 };
---
+++ src/configs/index.ts
@@ -0,0 +1,76 @@
+export const sidebarNavigation = [
+  {
+    label: "Broker Login",
+    children: [
+      { name: "Broker A" },
+      { name: "Broker B" },
+      { name: "Broker C" },
+    ],
+  },
+  {
+    label: "Instrument Selection",
+    children: [
+      { name: "Options" },
+      { name: "Index" },
+      { name: "Expiry Date" },
+      { name: "Equity" },
+    ],
+  },
+  {
+    label: "Customize Parameter",
+    children: [
+      { name: "Strike Size" },
+      { name: "Lot Size" },
+      { name: "Strike Length" },
+      { name: "Scan Time" },
+      { name: "PNL Check Frequency" },
+      { name: "Trading Decision" },
+      { name: "Buying" },
+      { name: "Selling" },
+    ],
+  },
+  {
+    label: "Decision",
+    children: [{ name: "IF_else" }],
+  },
+  {
+    label: "Loops",
+    children: [{ name: "For Loop" }, { name: "While Loop" }],
+  },
+  {
+    label: "Trading",
+    children: [
+      { name: "Instruments" },
+      { name: "Indicators" },
+      { name: "Triggers" },
+      { name: "Trade Legs" },
+    ],
+  },
+  {
+    label: "OMS",
+    children: [
+      { name: "Cancel Open Order" },
+      { name: "Square Off All Positions" },
+      { name: "Order Filter" },
+      { name: "Square Positions" },
+      { name: "Price Limit" },
+      { name: "Premium Above" },
+      { name: "Premium Below" },
+      { name: "All Buy" },
+      { name: "All Sell" },
+    ],
+  },
+  {
+    label: "Hedge Buy",
+    children: [{ name: "Premium Max Limit" }, { name: "Quantity" }],
+  },
+  {
+    label: "RMS",
+    children: [{ name: "PNL" }, { name: "Order Count" }],
+  },
+  {
+    label: "Trade Leg",
+    children: [{ name: "Strike Selection" }],
+  },
+  // Add more sections and children as needed
+];
