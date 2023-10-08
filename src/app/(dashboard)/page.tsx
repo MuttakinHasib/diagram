544d98ee8faf8f9283e99509324e01140fcf03f2
Muttakin Islam Hasib <muttakinislamhasib@gmail.com>
2023-10-08 23:14:15 +0600

    feat(web): :hammer: add useFlow hook, refactor types

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,86 +1,44 @@
 "use client";
-import Broker from "@/components/nodes";
-import { initialNodes } from "@/data/node";
-import React, { useCallback, useMemo, useState, useRef } from "react";
-import ReactFlow, {
-  Background,
-  Controls,
-  Edge,
-  MiniMap,
-  Node,
-  useEdgesState,
-  useNodesState,
-  useReactFlow,
-  addEdge,
-} from "reactflow";
-import { nanoid } from "nanoid";
+
+import React from "react";
+import ReactFlow, { Background, Controls, MiniMap } from "reactflow";
+import { useFlow } from "@/hooks/useFlow";
+const proOptions = { hideAttribution: true };
 
 type Props = {};
 
 const HomePage = (props: Props) => {
-  const reactFlowWrapper: any = useRef(null);
-  const { fitView } = useReactFlow();
-  const [reactFlowInstance, setReactFlowInstance] = useState<any>(null);
-  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
-  const [edges, setEdges, onEdgesChange] = useEdgesState([]);
-  const nodeTypes = useMemo(() => ({ broker: Broker }), []);
+  const {
+    nodes,
+    setNodes,
+    nodeTypes,
+    onNodesChange,
+    edges,
+    setEdges,
+    onEdgesChange,
+    onConnect,
+    reactFlowWrapper,
+    setReactFlowInstance,
+    onDrop,
+    onDragOver,
+  } = useFlow();
 
-  const proOptions = { hideAttribution: true };
-  const onConnect = useCallback(
-    (params: any) =>
-      setEdges((eds: Edge[]) =>
-        addEdge({ ...params, animated: true, style: { stroke: "#fff" } }, eds)
-      ),
-    []
-  );
-  const onDrop = useCallback(
-    (event: any) => {
-      event.preventDefault();
-
-      const reactFlowBounds = reactFlowWrapper.current.getBoundingClientRect();
-      const type = event.dataTransfer.getData("application/reactflow");
-
-      // check if the dropped element is valid
-      if (typeof type === "undefined" || !type) {
-        return;
-      }
-
-      const position = reactFlowInstance.project({
-        x: event.clientX - reactFlowBounds.left,
-        y: event.clientY - reactFlowBounds.top,
-      });
-      const newNode = {
-        id: "azim",
-        type,
-        position,
-        data: { label: `${type} node` },
-      };
-
-      setNodes((nds: Node) => nds.concat(newNode));
-    },
-    [reactFlowInstance]
-  );
-  const onDragOver = useCallback((event: any) => {
-    event.preventDefault();
-    event.dataTransfer.dropEffect = "move";
-  }, []);
   return (
-    <div
-      className="p-5 !h-[calc(100vh-5rem-64px)] reactflow-wrapper"
-      ref={reactFlowWrapper}
-    >
+    <div className="p-5 !h-[calc(100vh-5rem-64px)]" ref={reactFlowWrapper}>
       <ReactFlow
-        {...{ proOptions, nodeTypes, nodes, onNodesChange, onEdgesChange }}
-        // onNodeClick={onNodeClick}
-        // onNodeMouseEnter={handleMouseEnter}
-        onConnect={onConnect}
-        onDrop={onDrop}
+        {...{
+          proOptions,
+          nodeTypes,
+          nodes,
+          onNodesChange,
+          onEdgesChange,
+          onConnect,
+          onDrop,
+          onDragOver,
+        }}
         maxZoom={5}
         minZoom={0.3}
-        fitview="true"
         onInit={setReactFlowInstance}
-        onDragOver={onDragOver}
-        arrowheadcolor="#595A66"
         fitView
         className="border rounded-md"
       >
---
+++ src/hooks/useFlow.tsx
@@ -0,0 +1,81 @@
+import Broker from "@/components/nodes/broker";
+import { initialNodes } from "@/data/node";
+import { nanoid } from "nanoid";
+import { DragEvent, useCallback, useMemo, useRef, useState } from "react";
+import {
+  Edge,
+  Node,
+  ReactFlowInstance,
+  addEdge,
+  useEdgesState,
+  useNodesState,
+} from "reactflow";
+
+export const useFlow = () => {
+  const reactFlowWrapper = useRef<HTMLDivElement | null>(null);
+  const [reactFlowInstance, setReactFlowInstance] =
+    useState<ReactFlowInstance | null>(null);
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState([]);
+
+  const nodeTypes = useMemo(() => ({ broker: Broker }), []);
+
+  const onConnect = useCallback(
+    (params: any) =>
+      setEdges((eds: Edge[]) =>
+        addEdge({ ...params, animated: true, style: { stroke: "#fff" } }, eds)
+      ),
+    [setEdges]
+  );
+
+  const onDrop = useCallback(
+    (event: DragEvent<HTMLDivElement>) => {
+      event.preventDefault();
+
+      const reactFlowBounds =
+        reactFlowWrapper?.current?.getBoundingClientRect();
+      const type = event.dataTransfer.getData("application/reactflow");
+
+      if (typeof type === "undefined" || !type || !reactFlowBounds) {
+        return;
+      }
+
+      const position = reactFlowInstance?.project({
+        x: event.clientX - reactFlowBounds.left,
+        y: event.clientY - reactFlowBounds.top,
+      });
+
+      if (!position) return;
+
+      const newNode = {
+        id: nanoid(),
+        type,
+        position,
+        data: { label: `${type} node` },
+      };
+
+      setNodes((nds: Node[]) => nds.concat(newNode));
+    },
+    [reactFlowInstance, setNodes]
+  );
+
+  const onDragOver = useCallback((event: DragEvent<HTMLDivElement>) => {
+    event.preventDefault();
+    event.dataTransfer.dropEffect = "move";
+  }, []);
+
+  return {
+    nodes,
+    setNodes,
+    onNodesChange,
+    edges,
+    setEdges,
+    onEdgesChange,
+    onConnect,
+    onDrop,
+    onDragOver,
+    nodeTypes,
+    reactFlowWrapper,
+    setReactFlowInstance,
+  };
+};
